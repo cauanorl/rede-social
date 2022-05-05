45bd95e4d120519e3b2e072645259b4e8c89dc77
Cauanorl <rodriguescauan701@gmail.com>
2022-05-05 20:06:03 -0300

    adicionando as urls de trocar e de resetar a senha

--- account/views.py
+++ account/views.py
@@ -7,32 +7,32 @@ from . import models, forms
 
 
 # Create your views here.
-def user_login(request):
-    if request.method == "POST":
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = auth.authenticate(
-                request, username=username, password=password)
-            if user is not None:
-                if user.is_active:
-                    auth.login(request, user)
-                    return HttpResponse('Authenticado')
-                else:
-                    return HttpResponse('Conta desativada')
-            else:
-                return HttpResponse('Nome ou sneha invalidos.')
-    elif request.method == 'GET':
-        form = forms.LoginForm()
+# def user_login(request):
+#     if request.method == "POST":
+#         form = forms.LoginForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password')
+#             user = auth.authenticate(
+#                 request, username=username, password=password)
+#             if user is not None:
+#                 if user.is_active:
+#                     auth.login(request, user)
+#                     return HttpResponse('Authenticado')
+#                 else:
+#                     return HttpResponse('Conta desativada')
+#             else:
+#                 return HttpResponse('Nome ou sneha invalidos.')
+#     elif request.method == 'GET':
+#         form = forms.LoginForm()
     
-    return render(request, 'account/login.html', {'form': form})
+#     return render(request, 'account/login.html', {'form': form})
 
 
-def user_logout(request):
-    if request.user.is_authenticated:
-        auth.logout(request)
-    return redirect(reverse('account:login'))
+# def user_logout(request):
+#     if request.user.is_authenticated:
+#         auth.logout(request)
+#     return redirect(reverse('account:login'))
 
 
 def user_register(request):
@@ -41,4 +41,5 @@ def user_register(request):
 
 @login_required
 def user_dashboard(request):
-    render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    return render(request, 'registration/dashboard.html', {
+        'section': 'dashboard'})
